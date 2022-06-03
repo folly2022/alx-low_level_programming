08f909ae5f408873dfccd5527d69e653f84f84d8
Olarewaju Lawal <azlaw20@gmail.com>
2022-06-02 18:33:12 -0700

    Update printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,7 @@
 *
 * Return: Always 0
 */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+int main(void){
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 } 
