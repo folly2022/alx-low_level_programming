da36900e1e7678a32f8d069c80833b9006dd7aa3
Olarewaju Lawal <azlaw20@gmail.com>
2022-07-06 16:58:35 -0700

    What's my name

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+This project is about function pointers, when, why and how to use them in C-programming
+ 
+0. a function that prints a name.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
