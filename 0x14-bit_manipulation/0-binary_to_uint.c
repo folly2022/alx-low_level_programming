5865b19cb4038bf969f2175baefd67c138f57e0f
Olarewaju Lawal <105002145+folly2022@users.noreply.github.com>
2022-07-21 21:09:06 -0600

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x = 0;
+	unsigned int num = 0;
+	int len = 0;
 
-	if (b == NULL)
+	if (b[len] == '\0')
 		return (0);
-	while (*b)
+
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		if (*b == '1')
-			x = (x << 1) | 1;
-		else if (*b == '0')
-			x <<= 1;
-		else
-			return (0);
-		b++;
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
-	return (x);
+
+	return (num);
 }
